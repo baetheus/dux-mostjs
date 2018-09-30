9b738e8f13b1ac12bdf60aee1fbdfccc3988704d
Brandon Blaylock <brandon@null.pub>
2018-09-29 23:42:40 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dux-mostjs
+
+An experiment to port some of the functionality of ngrx into an agnostic redux-like pattern built on top of mostjs streams
---
+++ src/libs/dux-mostjs/metareducers.ts
@@ -0,0 +1,50 @@
+import { create } from 'most-subject';
+import { scan, map } from '@most/core';
+
+import {
+  MetaReducerModify,
+  MetaReducerAdd,
+  Reducer,
+  MetaReducer,
+} from './models';
+import { sink } from './utils';
+
+export const metaReducerFactory = <S>() => {
+  const [metaReducersModifySink, metaReducersModifyStream] = create<
+    MetaReducerModify<S>
+  >();
+  const metaReducersStream = scan(
+    (rs, nr) => {
+      switch (nr.change) {
+        case 'add':
+          return [...rs, nr];
+        case 'remove':
+          return rs.filter(r => r.key === nr.key);
+        default:
+          return rs;
+      }
+    },
+    [] as MetaReducerAdd<S>[],
+    metaReducersModifyStream
+  );
+  const metaReducerSink = sink<MetaReducerModify<S>>(metaReducersModifySink);
+
+  const metaReducerStream = map(
+    s =>
+      s.reduce(
+        (acc, mr): MetaReducer<S> => (r: Reducer<S>) => mr.metaReducer(acc(r)),
+        (r: Reducer<S>): Reducer<S> => r
+      ),
+    metaReducersStream
+  );
+  const addMetaReducer = (key: string, metaReducer: MetaReducer<S>) =>
+    metaReducerSink({ change: 'add', key, metaReducer });
+  const removeMetaReducer = (key: string) =>
+    metaReducerSink({ change: 'remove', key });
+
+  return {
+    metaReducerStream,
+    addMetaReducer,
+    removeMetaReducer,
+  };
+};
